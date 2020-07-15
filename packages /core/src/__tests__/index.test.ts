b107a614ee64d1ee0abf07198a9887ba76a810e9
yangxiaodong <yangxiaodong@qianmi.com>
2020-07-15 19:02:43 +0800

    add; 初始化添加起来..

--- packages /core/src/__tests__/index.test.ts
+++ packages /core/src/__tests__/index.test.ts
@@ -7,17 +7,38 @@
  * @Date    2020/7/15
  **/
 
-import {compile} from "../index";
+import {compile, compileExpress} from "../index";
 
 describe('基础编译', () => {
   it('基本转换', async () => {
-
     for (let i = 0, iLen = codes.length; i < iLen; i++) {
       let code1 = codes[i];
       let result  = compile(code1,{ast:true});
       debugger;
       expect(result.code).toMatchSnapshot("序号:"+i);
     }
+  });
+
+  it('表达式转换-数字', async () => {
+    //累似于.  输入常量 22 ==> (() => 111)()  这样属性赋值可以为  var a = (() => 111)();
+    let result = compileExpress("111");
+    expect(result.code).toEqual(`(async () => {
+  return 111;
+})();`);
+  });
+
+   it('表达式转换-await', async () => {
+      //累似于.  输入常量 22 ==> (() => 111)()  这样属性赋值可以为  var a = (() => 111)();
+      let result = compileExpress("await fetchApi('sdfsdf')");
+      expect(result.code).toEqual(`(async () => {
+  return await fetchApi('sdfsdf');
+})();`);
+
+     let result1 = compileExpress("console.log(1231);await fetchApi('sdfsdf')");
+     expect(result1.code).toEqual(`(async () => {
+  console.log(1231);
+  return await fetchApi('sdfsdf');
+})();`);
   });
 });
 
--- packages /core/src/index.ts
+++ packages /core/src/index.ts
@@ -9,6 +9,39 @@
 
 import * as babel from  '@babel/core';
 import {TransformOptions} from "@babel/core";
+import  template from '@babel/template'
+
+
+
+export function compileExpress(code:string,opt?:TransformOptions){
+  //是不是要把最后一行添加一个return;
+
+  let wrapSource = `(async ()=>{
+${code}
+})()`;
+  return compile(wrapSource,{
+    plugins: [{
+      visitor:  {
+        ExpressionStatement(path){
+          let isEndExp = path.node.end==wrapSource.length-5;
+          if(isEndExp) {
+            debugger;
+            let result  = path.getSource();
+            if(!result.includes('return')) {
+              path.replaceWith( babel.template.statement.ast`return ${path.getSource()}`);
+            }
+          }
+        },
+        BlockStatement(path){
+          //path.getSource()
+          // path.replaceWithSourceString(``)
+          debugger;
+        }
+      }
+    }]
+  });
+}
+
 
 export function compile(code:string,opt?:TransformOptions){
   return babel.transform(code, opt);
