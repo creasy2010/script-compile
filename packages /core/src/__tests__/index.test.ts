5dc59c2af947013c722323aecd314398ce00e276
yangxiaodong <yangxiaodong@qianmi.com>
2020-07-15 16:55:05 +0800

    add: 初始化项目

---
+++ packages /core/src/__tests__/index.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @desc
+ *
+ * @使用场景
+ *
+ * @coder.yang2010@gmail.com
+ * @Date    2020/7/15
+ **/
+
+describe('11', () => {
+  it('11', async () => {
+    expect('hello').toEqual("hello");
+  });
+});
