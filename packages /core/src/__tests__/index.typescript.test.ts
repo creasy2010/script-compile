72ca8c4fc40e1ae28b94c748b80a31ea69702969
yangxiaodong <yangxiaodong@qianmi.com>
2020-07-17 15:37:57 +0800

    add: 添加起来..

--- packages /core/src/__tests__/index.typescript.test.ts
+++ packages /core/src/__tests__/index.typescript.test.ts
@@ -7,15 +7,29 @@
  * @Date    2020/7/16
  **/
 
+import { compileExpress } from "../index";
 
-import {compileExpress} from "../index";
+describe("typescript类型验证", () => {
+  it("简单类型", async () => {
+    let result1 = compileExpress("111 as number", { ast: true });
+    expect(result1.code).toMatchInlineSnapshot(`
+      "\\"use strict\\";
 
-describe('typescript类型验证', () => {
-  it('简单类型', async () => {
-    let result1 = compileExpress("111 as number",{ast:true});
-    debugger;
-    expect(result1.code).toEqual(`(async () => {
-  return 111 as number;
-})();`);
+      (async () => {
+        return 111;
+      })();"
+    `);
+  });
+  it("option channel", async () => {
+    let result1 = compileExpress("a.b?.c?.d", { ast: true });
+    expect(result1.code).toMatchInlineSnapshot(`
+      "\\"use strict\\";
+
+      (async () => {
+        var _a$b, _a$b$c;
+
+        return (_a$b = a.b) === null || _a$b === void 0 ? void 0 : (_a$b$c = _a$b.c) === null || _a$b$c === void 0 ? void 0 : _a$b$c.d;
+      })();"
+    `);
   });
 });
--- packages /core/src/index.ts
+++ packages /core/src/index.ts
@@ -11,6 +11,29 @@ import * as babel from  '@babel/core';
 import {TransformOptions} from "@babel/core";
 import  template from '@babel/template'
 import PresetTypescript from "@babel/preset-typescript";
+import PresetEnv from "@babel/preset-env";
+import  PluginOptionalChaining from "@babel/plugin-proposal-optional-chaining";
+import  PluginTransform from "@babel/plugin-transform-typescript";
+
+import * as babelParser from '@babel/parser';
+
+
+
+
+let mockApi ={
+  assertVersion:()=>true
+};
+let presets  =  [
+  [PresetEnv,{
+    targets: {
+      "chrome": "60",
+    }
+  }],
+  PresetTypescript(mockApi)
+];
+let plugins = [
+  PluginTransform(mockApi,{}),
+  PluginOptionalChaining(mockApi,{}),]
 
 
 export function compileExpress(code:string,opt?:TransformOptions){
@@ -19,28 +42,32 @@ export function compileExpress(code:string,opt?:TransformOptions){
   let wrapSource = `(async ()=>{
 ${code}
 })()`;
+
   return compile(wrapSource,{
     ...opt,
     filename:"auto-gene/script.ts",
-    presets: [PresetTypescript],
-    plugins: [{
+    presets,
+    plugins: [
+      {
       visitor:  {
         ExpressionStatement(path){
           let isEndExp = path.node.end==wrapSource.length-5;
           if(isEndExp) {
             let result  = path.getSource();
             if(!result.includes('return')) {
-              path.replaceWith( babel.template.statement.ast`return ${path.getSource()}`);
+              debugger;
+
+              let result  =babelParser.parse(`return ${path.getSource()}`,{
+                allowAwaitOutsideFunction:true,
+                allowReturnOutsideFunction:true,
+                plugins:['typescript']
+              })
+              path.replaceWith( result.program.body[0]);
             }
           }
-        },
-        BlockStatement(path){
-          //path.getSource()
-          // path.replaceWithSourceString(``)
-          debugger;
         }
       }
-    }]
+    }].concat(plugins)
   });
 }
 
